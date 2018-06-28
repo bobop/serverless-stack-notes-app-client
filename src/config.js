e01fec2fae2ae4190870421117960a4cb4cda6ff
Ben Gibbs <ben@bobop.co.uk>
2018-06-27 20:18:27 -0700

    Setup AWS Amplify config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ca-central-1",
+    BUCKET: "bobop-serverless-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://7qnrw7wso7.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_wWjTF8iC3",
+    APP_CLIENT_ID: "7fj85f8vbl2mq6rb0uu0o5hltu",
+    IDENTITY_POOL_ID: "us-east-2:74455856-82aa-4074-907c-9c0f39df66f4"
+  }
+};
